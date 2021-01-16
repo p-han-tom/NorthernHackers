55e35e9e28077221b1fb930fa66429ebbd4f4403
JerryLin1 <jerreelynn@gmail.com>
2021-01-16 09:21:14 -0500

    made prefab

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class Player : NetworkedBehaviour
+{
+
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void CheckInput() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (IsLocalPlayer) {
+            CheckInput();
+            Move(); 
+        }
+    }
+
+    void Move()
+    {
+        rb.velocity = movement * 2f;
+
+        if (rb.velocity != Vector2.zero)
+        {
+            transform.rotation = (movement.x < 0) ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+}
