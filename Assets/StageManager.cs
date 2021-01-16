e58db830a9c0bc4bc634b7d8c23ceb1b792b74e1
p-han-tom <hantom0127@gmail.com>
2021-01-16 15:39:23 -0500

    beaver sky fall spawn in 10 second intervals

--- Assets/StageManager.cs
+++ Assets/StageManager.cs
@@ -12,10 +12,15 @@ public class StageManager : MonoBehaviour
     public bool initiate;
     public LayerMask treeLayer;
 
+    private float beaverSpawnTimer = 8f;
+    private GameObject beaverInstance;
+    private Vector2 beaverSpawnPos;
+    private bool beaverSpawned = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        beaverSpawned = false;
     }
 
     // Update is called once per frame
@@ -24,9 +29,47 @@ public class StageManager : MonoBehaviour
         if (initiate) {
             initiate = false;
             GenerateStage();
-            GameObject beaverInstance = Instantiate(beaver, new Vector3(5,0,0), Quaternion.identity);
-            beaverInstance.GetComponent<NetworkedObject>().Spawn();
+            
         }
+
+        beaverSpawnTimer += Time.deltaTime;
+        if (beaverSpawnTimer > 10f) {
+            beaverSpawnTimer = 0f;
+            beaverSpawnPos.x = Random.Range(-5f,5f);
+            beaverSpawnPos.y = Random.Range(-2f,2f);
+
+            Collider2D[] treeCheck = Physics2D.OverlapCircleAll(new Vector3(beaverSpawnPos.x,beaverSpawnPos.y,0), .75f, treeLayer);
+            while (treeCheck.Length>1) {
+                beaverSpawnPos.x = Random.Range(-5f,5f);
+                beaverSpawnPos.y = Random.Range(-2f,2f);
+
+                treeCheck = Physics2D.OverlapCircleAll(new Vector3(beaverSpawnPos.x,beaverSpawnPos.y,0), .75f, treeLayer);
+            }
+            beaverInstance = Instantiate(beaver, new Vector3(beaverSpawnPos.x,beaverSpawnPos.y+20f,0), Quaternion.identity);
+            beaverInstance.GetComponent<NetworkedObject>().Spawn();
+
+            beaverInstance.GetComponent<CapsuleCollider2D>().enabled = false;
+            beaverInstance.GetComponent<CircleCollider2D>().enabled = false;
+
+            beaverInstance.GetComponent<Rigidbody2D>().gravityScale = .5f;
+            beaverInstance.GetComponent<BeaverAi>().enabled = false;
+            beaverSpawned = true;
+        }
+
+        if (beaverSpawned == true) {
+            if (beaverInstance.transform.position.y <= beaverSpawnPos.y) {
+                beaverInstance.GetComponent<CapsuleCollider2D>().enabled = true;
+                beaverInstance.GetComponent<CircleCollider2D>().enabled = true;
+
+                beaverInstance.GetComponent<Rigidbody2D>().gravityScale = 0f;
+                beaverInstance.GetComponent<BeaverAi>().enabled = true;
+
+                beaverSpawned = false;
+            }
+            
+        
+        }
+
     }
 
     void GenerateStage() {
@@ -51,7 +94,6 @@ public class StageManager : MonoBehaviour
                 treeInstance.GetComponent<NetworkedObject>().Spawn();
             }
             spawnTree = true;
-            
 
         }
     }
