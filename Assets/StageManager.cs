d9213910ac4a3e5ccf1ab672681a35388006a220
p-han-tom <hantom0127@gmail.com>
2021-01-16 13:45:44 -0500

    level generation

--- Assets/StageManager.cs
+++ Assets/StageManager.cs
@@ -9,6 +9,7 @@ public class StageManager : MonoBehaviour
     public GameObject tree;
     public GameObject rocks;
     public bool initiate;
+    public LayerMask treeLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +22,36 @@ public class StageManager : MonoBehaviour
     {
         if (initiate) {
             initiate = false;
-            GameObject treeInstance = Instantiate(tree, new Vector3(5,0,0), Quaternion.identity);
-            treeInstance.GetComponent<NetworkedObject>().Spawn();
+            GenerateStage();
+            // GameObject treeInstance = Instantiate(tree, new Vector3(5,0,0), Quaternion.identity);
+            // treeInstance.GetComponent<NetworkedObject>().Spawn();
+        }
+    }
+
+    void GenerateStage() {
+        // maxmimum of 32 trees
+        float x;
+        float y;
+        bool spawnTree = true;
+        for (int i = 0; i < 24; i ++) {
+            x = Random.Range(-8f,8f);
+            y = Random.Range(-3.5f,3.5f);
+
+            Collider2D[] treeCheck = Physics2D.OverlapCircleAll(new Vector3(x,y,0), .75f, treeLayer);
+            foreach (Collider2D entity in treeCheck) {
+                if (entity.gameObject.layer == LayerMask.NameToLayer("Tree")) {
+                    spawnTree = false;
+                    break;
+                }
+            }
+
+            if (spawnTree) {
+                GameObject treeInstance = Instantiate(tree, new Vector3(x,y,0), Quaternion.identity);
+                treeInstance.GetComponent<NetworkedObject>().Spawn();
+            }
+            spawnTree = true;
+            
+
         }
     }
 }
