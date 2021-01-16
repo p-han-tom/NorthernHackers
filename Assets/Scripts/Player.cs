de4cc2b7c81cc50939ad83dd9b870fe4eac8b7c4
p-han-tom <hantom0127@gmail.com>
2021-01-16 12:10:01 -0500

    networked tree chopping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,18 +9,37 @@ public class Player : NetworkedBehaviour
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator animator;
+    private Transform attackPoint;
+
+    public LayerMask treeLayer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        attackPoint = transform.Find("AttackPoint");
     }
 
     void CheckInput() {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetMouseButtonDown(0)) {
+            // animator.SetTrigger("hack");
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePos-transform.position).normalized;
+            transform.rotation = (direction.x < 0) ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
+            
+            // logic for creating overlap circle
+            Collider2D[] hitEntities = Physics2D.OverlapCircleAll(attackPoint.position, 0.5f, treeLayer);
+            foreach (Collider2D entity in hitEntities) {
+                entity.GetComponent<Tree>().TakeDamage();
+            }
+        }
     }
+
 
     // Update is called once per frame
     void Update()
