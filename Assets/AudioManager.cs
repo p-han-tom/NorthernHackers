776ecf35ee4649a36ee0145cfbc7272c93d1fd84
p-han-tom <hantom0127@gmail.com>
2021-01-16 19:01:53 -0500

    sound

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -3,10 +3,14 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using MLAPI;
+using MLAPI.Messaging;
+using MLAPI.NetworkedVar;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : NetworkedBehaviour
 {
     public Sound[] sounds;
+
 
     void Awake() {
         foreach(Sound s in sounds) {
@@ -18,6 +22,19 @@ public class AudioManager : MonoBehaviour
     }
 
     public void Play(string name) {
+        InvokeServerRpc(Play1,name);
+        InvokeClientRpcOnEveryone(Play1, name);
+    }
+
+    public void Stop(string name) {
+        InvokeServerRpc(Stop1, name);
+        InvokeClientRpcOnEveryone(Stop1, name);
+    }
+
+
+    [ServerRPC]
+    public void Play1(string name) {
+        
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if (s==null) {
             Debug.Log("no such sound as " + name);
@@ -27,7 +44,8 @@ public class AudioManager : MonoBehaviour
         
     }
 
-    public void Stop(string name) {
+    [ServerRPC]
+    public void Stop1(string name) {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if (s==null || !s.source.isPlaying) {
             return;
